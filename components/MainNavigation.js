8aec435a4403ba41aa2f2938ca0342de907c63d0
dylansu <hs3225@columbia.edu>
2022-01-24 10:20:53 -0500

    add search feature

--- components/MainNavigation.js
+++ components/MainNavigation.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import Detail from '../screens/Detail'
 import NavBar from './NavBar';
+import Search from '../screens/Search';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +17,10 @@ class MainNavigation extends React.PureComponent {
           header: ({navigation}) => <NavBar navigation={navigation} main={true} />
         }}/>
         <Stack.Screen name="Detail" component={Detail} options={{
+          headerTransparent: true,
+          header: ({navigation}) => <NavBar navigation={navigation} />
+        }}/>
+        <Stack.Screen name="Search" component={Search} options={{
           headerTransparent: true,
           header: ({navigation}) => <NavBar navigation={navigation} />
         }}/>
